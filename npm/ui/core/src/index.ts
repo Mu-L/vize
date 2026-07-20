ee5d5b97712396a64e4ddeee929f00895a11bb83
ubugeeei <ubuge1122@gmail.com>
2026-07-20 14:34:02 +0900

    feat(ui): add accessible button primitive (#3177)

--- npm/ui/core/src/index.ts
+++ npm/ui/core/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./controllable-state.ts";
 /** Visually hidden content exposed to assistive technology. */
+export * from "./button.ts";
 export * from "./primitive.ts";
 export { default as VisuallyHidden } from "./VisuallyHidden.vue";
